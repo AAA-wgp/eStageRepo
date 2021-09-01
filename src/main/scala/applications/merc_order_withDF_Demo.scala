0fa9c48c948c8dba37e24bd141d19e6303fbf10d
xiaopao <wangguoping@hrfax.cn>
2021-09-01 17:50:25 +0800

    由每天统计修改为按月统计指标，对应分期放款率以百分号形式展示

--- src/main/scala/applications/merc_order_withDF_Demo.scala
+++ src/main/scala/applications/merc_order_withDF_Demo.scala
@@ -2,7 +2,7 @@ package applications
 
 import bean.{Buff, Merc_ResultInfo}
 import controller.collect.{ExportData, GetMercShopDetailInfo}
-import org.apache.spark.sql.{DataFrame, Encoder, Encoders, functions}
+import org.apache.spark.sql.{DataFrame, Dataset, Encoder, Encoders, functions}
 import org.apache.spark.sql.expressions.Aggregator
 import org.apache.spark.sql.functions._
 import utils.Sparkutils
@@ -60,11 +60,11 @@ object merc_order_withDF_Demo {
       .withColumnRenamed("group_id","platform_code")
       .withColumnRenamed("store_id","shop_id")
       .withColumnRenamed("lending_time","loan_success_time")
-      .withColumn("loan_success_time",substring(col("loan_success_time"),1,10))
+      .withColumn("loan_success_time",substring(col("loan_success_time"),1,7))
 
     //    以DF形式获取订单分期贷款信息表数据
     val frame_merc_loan = ExportData.getTableAsDF("merc_order_loan", cols_merc_loan)
-      .withColumn("loan_success_time",substring(col("loan_success_time"),1,10))
+      .withColumn("loan_success_time",substring(col("loan_success_time"),1,7))
 
     //   以DF形式获取账户信息表字段数组
     val frame_tb_role = ExportData.getTableAsDF("tb_account", cols_tb_account)
@@ -100,17 +100,27 @@ object merc_order_withDF_Demo {
       .union(frame_business)
 
     //将合并后数据关联门店展平后详细数据获取对应一级部门、二级部门等信息
-    val detailInfo: DataFrame = frame_union_order_info.join(
+    val detailInfo = frame_union_order_info.join(
       frame_merc_detail_info,Seq("shop_id"),"left"
     ).drop(frame_merc_detail_info("shop_id"))
+      .selectExpr(
+        "order_no",
+        "shop_id",
+        "loan_or_not",
+        "by_stages",
+        "platform_code",
+        "name_of_salesman",
+        "loan_success_time",
+        "case loan_or_not when '是' then( case by_stages when '分期' then '分期且放款'  else '' end) else '' end as stageAndLoan"
+      )
 
     //注册自定义函数
     spark.udf.register("avgStageLoan", functions.udaf( new GetStageRatioFunction))
 
     //计算得出分组后详细结果
     detailInfo.repartition(1)
-      .groupBy("shop_id","loan_success_time")
-      .agg("order_no"->"avgStageLoan")
+      .groupBy("loan_success_time","shop_id")
+      .agg("stageAndLoan"->"avgStageLoan")
       .show(20,truncate = false)
 
     //关闭sparkSession连接
@@ -118,13 +128,13 @@ object merc_order_withDF_Demo {
   }
 
     //自定义udaf获取门店对应放款时间的分期放款销量占比
-  class GetStageRatioFunction  extends Aggregator[Merc_ResultInfo,Buff,Double]{
+  class GetStageRatioFunction  extends Aggregator[String,Buff,String]{
     override def zero: Buff = Buff(0,0)
 
     //根据Merc_ResultInfo表by_stages与loan_or_not字段判断订单是否为分期且放款
-    override def reduce(b: Buff, a: Merc_ResultInfo): Buff = {
+    override def reduce(b: Buff, a: String): Buff = {
       b.sum += 1
-      if(a.by_stages == "分期" && a.loan_or_not == "是"){
+      if(a == "分期且放款" ){
         b.stageAndLoan += 1
       }
       b
@@ -136,11 +146,11 @@ object merc_order_withDF_Demo {
       b1
     }
 
-    override def finish(reduction: Buff): Double = reduction.stageAndLoan.toDouble/reduction.sum.toDouble
+    override def finish(reduction: Buff): String = (((reduction.stageAndLoan*1000).toDouble/ reduction.sum.toDouble)/10).toString.concat("%")
 
     override def bufferEncoder: Encoder[Buff] = Encoders.product
 
-    override def outputEncoder: Encoder[Double] = Encoders.scalaDouble
+    override def outputEncoder: Encoder[String] = Encoders.STRING
   }
 
 }
